728f7567961127aa1f136ca0fb2b00bbd633bfa4
Hank's Mac <hankhplee@gmail.com>
2017-12-14 15:54:18 +0800

    add display and update dashborad

--- app.py
+++ app.py
@@ -4,27 +4,48 @@ app = Flask(__name__)
 
 global state
 state = "VEVLET"
+location = "Leave Home"
+environment = {
+    "lightness": 0.0,
+    "temperature":0.0,
+    "humidity":0.0,
+    "volume":0.0
+}
 
 @app.route('/')
 def dashboard():
     global state
-    return render_template('dashboard.html', state=state)
+    return render_template('dashboard.html', state=state, location=location)
 
-@app.route('/send_data', methods=['POST'])
+@app.route('/sendData', methods=['POST'])
 def send_data():
     if request.method == 'POST':
         data = request.form['test']
         print(data)
         return data
 
-@app.route('/get_location', methods=['GET'])
-def get_locatoin():
+@app.route('/getState', methods=['GET'])
+def getLocatoin():
     print(state)
     return state
 
-@app.route('/change_state', methods=['POST'])
-def change_state():
+@app.route('/changeState', methods=['POST'])
+def changeState():
     global state
     state = request.form['state']
     print(state)
     return state
+
+@app.route('/changeLocation', methods=['POST'])
+def changeLocation():
+    global location
+    location = request.form['location']
+    state = "VELVET"
+    print(location)
+    return render_template('dashboard.html', state=state, location=location)
+
+@app.route('/display', methods=['GET'])
+def getData():
+    global environment
+    print(environment)
+    return render_template('display.html', environment = environment)
